48c75dd5d93095736e20c1a63bd2d0bf720278dc
AdharshSasidharan <103949764+AdharshSasidharan@users.noreply.github.com>
2023-03-17 17:28:53 +0530

    Update products.service.ts

--- products.service.ts
+++ products.service.ts
@@ -12,10 +12,23 @@ export class ProductsService {
     @InjectRepository(Products) private productRepository: Repository<Products>,
   ) {}
 
+  /**
+   * Get a product by ID.
+   *
+   * @param productId The ID of the product to retrieve.
+   * @returns The product with the given ID, or `null` if it doesn't exist.
+   */
   async getProducts(ProductID: number): Promise<Products> {
     return this.productRepository.findOneBy({ ProductID });
   }
 
+  
+  /**
+   * Get all products, optionally sorted by a given field.
+   *
+   * @param sort The field to sort by, if any.
+   * @returns An array of products, possibly sorted by the given field.
+   */
   async getAllProducts(sort: string | undefined): Promise<Products[]> {
     let products = await this.productRepository.find();
     if (sort) {
@@ -24,18 +37,39 @@ export class ProductsService {
     return products;
   }
 
+  
+   /**
+   * Create a new product.
+   *
+   * @param createProductDto The data for the new product.
+   * @returns The newly created product.
+   */
   async createProducts(ProductDetails: CreateProductDto): Promise<Products> {
     const newProduct = this.productRepository.create({ ...ProductDetails, createdAt: new Date() });
     await this.productRepository.save(newProduct);
     return newProduct;
   }
 
+  
+  /**
+   * Update a product by ID.
+   *
+   * @param productId The ID of the product to update.
+   * @param updateProductDto The new data for the product.
+   * @returns The updated product, or `null` if it doesn't exist.
+   */
   async updateProducts(ProductID: number, updateProductDetails: UpdateProductDto): Promise<Products> {
     await this.productRepository.update({ ProductID }, { ...updateProductDetails, updatedAt: new Date() });
     const updatedProduct = await this.productRepository.findOneBy({ ProductID });
     return updatedProduct;
   }
 
+    /**
+   * Delete a product by ID.
+   *
+   * @param productId The ID of the product to delete.
+   * @returns `true` if the product was deleted, or `false` if it doesn't exist.
+   */
   async deleteProducts(ProductID: number) {
     return await this.productRepository.delete({ ProductID });
   }
