2073e6897b02ad7c5f8d2185dc4309bba416a9b0
AdharshSasidharan <103949764+AdharshSasidharan@users.noreply.github.com>
2023-03-18 17:00:18 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from '@nestjs/core';// Importing the NestFactory class from the '@nestjs/core' module
+import { AppModule } from './app.module';// Importing the AppModule which is the root module of the Nest application
+// Defining an asynchronous function called 'bootstrap'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);// Creating an instance of the Nest application by passing in the AppModule to the create() method of the NestFactory class
+  await app.listen(3000);// Starting the Nest application by calling the listen() method and specifying the port number (3000 in this case)
 }
-bootstrap();
+bootstrap();// Calling the 'bootstrap' function to start the Nest application
